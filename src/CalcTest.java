b7c3e68e420ebb788ad3e0af2db4af7b1416b057
BeantGit <er.beantsaini@gmail.com>
2023-10-21 17:59:54 +0530

    Addition function have been added to CalcTest.

--- src/CalcTest.java
+++ src/CalcTest.java
@@ -14,6 +14,8 @@ public class CalcTest {
 	public static void main(String[] args) {
 		
 		System.out.println("My Calculator test.");
+		MyCalc c = new MyCalc();
+		System.out.println("Sum = "+c.add(10, 20));
 	}
 
 }
