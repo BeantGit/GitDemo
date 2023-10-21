aa6f60309d232079e1f3211e4ee52bbe0d84e01c
BeantGit <er.beantsaini@gmail.com>
2023-10-21 17:56:17 +0530

    Class MyCalc added to CalcTest java file.

--- src/CalcTest.java
+++ src/CalcTest.java
@@ -1,4 +1,14 @@
-
+class MyCalc
+{
+	int add(int a, int b)
+	{
+		return a+b;
+	}
+	int sub(int a, int b)
+	{
+		return a-b;
+	}
+}
 public class CalcTest {
 	
 	public static void main(String[] args) {
